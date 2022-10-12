f657752399829dfad36a965fe996e7b1e6a16fc2
bbordere <bastien.bordereau@gmail.com>
2022-10-12 18:38:55 +0200

    simplification replacer

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,47 +6,58 @@
 /*   By: bbordere <bbordere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 16:08:29 by bbordere          #+#    #+#             */
-/*   Updated: 2022/07/04 10:43:00 by bbordere         ###   ########.fr       */
+/*   Updated: 2022/10/12 18:12:07 by bbordere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
-#include <iostream>
-#include <string>
-#include "Replacer.hpp"
-  
-
-int main(int ac, char const **av)
+int main(int ac, char **av)
 {
 	if (ac != 4)
 	{
 		std::cout << "Wrong number of arguments !" << std::endl;
 		return (1);
 	}
-	std::string line;
-	Replacer replacer(av[1], av[2], av[3]);
-	if (!replacer.set_infile(replacer.get_filename()))
-		return (1);
-	if (!replacer.set_outfile(replacer.get_filename().append(".replace")))
-	{
-		replacer.get_infile().close();
-		return (1);
-	}
-	if (replacer.get_search().empty())
+	std::string		infileName(av[1]);
+	std::string		search(av[2]);
+	std::string		replace(av[3]);
+	std::string		line;
+	std::ifstream	inFile;
+	std::ofstream	outFile;
+
+
+	if (search.empty())
 	{
 		std::cout << "Search str can't be empty" << std::endl;
 		return (1);
 	}
-	while (std::getline(replacer.get_infile(), line))
+	inFile.open(infileName.c_str(), std::ifstream::in);
+	if (!inFile.is_open())
 	{
-		size_t pos = -1;	
-		while ((pos = line.find(replacer.get_search(), pos + 1)) != std::string::npos)
-		{
-			line.erase(pos, replacer.get_search().length());
-			line.insert(pos, replacer.get_replace());
-		}
-		replacer.get_out_file() << line << std::endl;
+		std::cout << "Error while opening file !" << std::endl;
+		return (1);
 	}
+	outFile.open((infileName.append(".replace")).c_str(), std::ofstream::out);
+	if (!outFile.is_open())
+	{
+		inFile.close();
+		std::cout << "Error while opening file !" << std::endl;
+		return (1);
+	}
+	while (std::getline(inFile, line))
+	{
+		size_t pos = -1;
+		while ((pos = line.find(search, pos + 1)) != std::string::npos)
+		{
+			line.erase(pos, search.length());
+			line.insert(pos, replace);
+		}
+		outFile << line << std::endl;
+	}
+	inFile.close();
+	outFile.close();
 	return (0);
 }
+
